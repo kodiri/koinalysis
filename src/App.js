dd6a1a5689976f20f7cc5098d738c5dec1273e2f
initgit <bala.chalasani@initfinity.com>
2019-02-06 14:08:29 +0000

    Update title to Koinalysis (#1)
    All done

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            <h1>Welcome to Koinalysis</h1>
           </p>
           <a
             className="App-link"
