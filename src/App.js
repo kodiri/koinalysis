f5c2908c1d79877838be31be8b7f512c2ef0d714
Natoshi Sakamoto <45171676+natoshi-sakamoto@users.noreply.github.com>
2019-02-07 21:42:37 +0000

    New dependencies and css. (#7)
    * Removed React boiler-plate
    
    * Installed axiom dependency and updated css
    
    * Added number format package.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+var NumberFormat = require('react-number-format');
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cryptos: []
+    };
+  }
+
+  loadMyAxios = () => {
+    axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,XRP,ETH,EOS,BCH&tsyms=USD')
+      .then(res => {
+        const cryptos = res.data;
+        this.setState({ cryptos: cryptos });
+      })
+  };
+
+  componentDidMount() {
+    this.loadMyAxios();
+
+  }
+
+  componentDidUpdate() {
+    this.loadMyAxios();
+  }
+
   render() {
     return (
       <div className="App">
-            <h1>Welcome to Koinalysis</h1>
-            <h1>Team App</h1>
-         </div>
+        <h1 id="header">Welcome to Koinalysis!!!!</h1>
+        <h1 id="header">Team App</h1>
+        {Object.keys(this.state.cryptos).map((key) => (
+          <div id="crypto-data">
+            <span className="left">{key}</span>
+            <span className="right"><NumberFormat 
+              value={this.state.cryptos[key].USD} 
+              displayType={'text'} decimalPrecision={2} 
+              thousandSeparator={true} prefix={'$'} />
+            </span>
+          </div>
+        ))}
+      </div>
     );
   }
 }
