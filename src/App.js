68252aa08bfd9f9dee4bce3b04eeacd9c7fdea8b
Rafael Ballestrin <rafaelballestrin@gmail.com>
2019-02-07 17:25:48 +0000

    Add second title 'team app'

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             <h1>Welcome to Koinalysis</h1>
+            <h1>Team App</h1>
           </p>
           <a
             className="App-link"
