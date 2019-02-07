6f5b44a65859e3ccfe813aa26ebf01c513629b5f
ajpereira02 <38704306+ajpereira02@users.noreply.github.com>
2019-02-07 17:00:12 +0000

    Ashley test commit (#5)
    gr8

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            <h2>Welcome to Koinalysis!!!</h2>
+            <h2>Welcome to Koinalysis!!!!</h2>
           </p>
           <a
             className="App-link"
