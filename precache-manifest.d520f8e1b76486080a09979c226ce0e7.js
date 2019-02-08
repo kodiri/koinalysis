600318613dcce0142ce1bf4ae9eb6d06517653ab
Ricardo Mallols <ricardo.mallols@gmail.com>
2019-02-08 12:08:38 +0000

    Updates

---
+++ precache-manifest.d520f8e1b76486080a09979c226ce0e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e772313265f92855104",
+    "url": "/koinalysis/static/js/runtime~main.7e772313.js"
+  },
+  {
+    "revision": "056ef7ae1404c4198baf",
+    "url": "/koinalysis/static/js/main.056ef7ae.chunk.js"
+  },
+  {
+    "revision": "0e3e041939adbcfb8229",
+    "url": "/koinalysis/static/js/1.0e3e0419.chunk.js"
+  },
+  {
+    "revision": "056ef7ae1404c4198baf",
+    "url": "/koinalysis/static/css/main.618d79ab.chunk.css"
+  },
+  {
+    "revision": "3daeacd2e31782f30bc998cd7f5acba1",
+    "url": "/koinalysis/index.html"
+  }
+];
