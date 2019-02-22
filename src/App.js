689a9357474ea4f1cf95b10b078318cb9ba6ea7c
initgit <bala.chalasani@initfinity.com>
2019-02-22 14:19:55 +0000

    Fixes to the text, favicon, added a tracker, resized the widgets, better fit (#32)
    * With new widgets from coinlib
    
    * Fixed page title, favicon, added coin tracker bar at top, sizing issue fixed
    
    * After resolving merge conflicts with upstream

--- src/App.js
+++ src/App.js
@@ -5,8 +5,19 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="Coinlib-tracker">
+          <div className="Coinlib-tracker-child">
+            <iframe title="Coinlib-tracker-title" className="Coinlib-tracker-iframe" src="https://widget.coinlib.io/widget?type=horizontal_v2&theme=dark&pref_coin_id=1508&invert_hover=no" width="100%" height="36" scrolling="auto" marginWidth="0" marginHeight="0" frameBorder="0" border="0">
+            </iframe></div>
+          <div className="Coinlib-tracker-bottom">
+            powered by&nbsp;
+            <a href="https://coinlib.io" target="_blank" rel="noopener noreferrer" className="Coinlib-tracker-link">
+                      Coinlib
+            </a>
+          </div>
+        </div>
+        <h2>Koinalysis</h2>
         <header className="App-header">
-          <h3>Koinalysis</h3>
           <div className="Dashboard">
             <div className="Dashboard-child">
               <iframe title="Coinlib-iframe" className="Dashboard-iframe" src="https://widget.coinlib.io/widget?type=full_v2&theme=dark&cnt=5&pref_coin_id=1508&graph=yes" width="100%" height="409" scrolling="auto" marginWidth="0" marginHeight="0" frameBorder="0" border="0">
@@ -24,7 +35,7 @@ class App extends Component {
           <div className="CurrencyWidget-bottom">
             Koinalysis powered by&nbsp;
             <a href="https://coinlib.io" target="_blank" rel="noopener noreferrer" className="Coinlib-link">
-                  Coinlib
+              Coinlib
             </a>
           </div>
         </div>
@@ -36,10 +47,10 @@ class App extends Component {
           <div className="CurrencyWidget-bottom">
             Koinalysis powered by&nbsp;
             <a href="https://coinlib.io" target="_blank" rel="noopener noreferrer" className="Coinlib-link">
-                  Coinlib
+              Coinlib
             </a>
           </div>
-        </div>  
+        </div>
         <div className="CurrencyWidget">
           <div className="CurrencyWidget-child">
             <iframe title="CoinlibWidget-iframe" src="https://widget.coinlib.io/widget?type=chart&theme=dark&coin_id=619&pref_coin_id=1508" width="100%" height="536" scrolling="auto" marginWidth="0" marginHeight="0" frameBorder="0" border="0" className="CurrencyWidget-iframe">
@@ -48,7 +59,7 @@ class App extends Component {
           <div className="CurrencyWidget-bottom">
             Koinalysis powered by&nbsp;
             <a href="https://coinlib.io" target="_blank" rel="noopener noreferrer" className="Coinlib-link">
-                  Coinlib
+              Coinlib
             </a>
           </div>
         </div>
@@ -60,7 +71,7 @@ class App extends Component {
           <div className="CurrencyWidget-bottom">
             Koinalysis powered by&nbsp;
             <a href="https://coinlib.io" target="_blank" rel="noopener noreferrer" className="Coinlib-link">
-                  Coinlib
+              Coinlib
             </a>
           </div>
         </div>
@@ -72,7 +83,7 @@ class App extends Component {
           <div className="CurrencyWidget-bottom">
             Koinalysis powered by&nbsp;
             <a href="https://coinlib.io" target="_blank" rel="noopener noreferrer" className="Coinlib-link">
-                  Coinlib
+              Coinlib
             </a>
           </div>
         </div>
