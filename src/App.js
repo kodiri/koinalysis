0b0ca8b7fd187d77195b5a2b91a960bd3f51484e
Ashley Pereira <38704306+ajpereira02@users.noreply.github.com>
2019-02-26 14:51:30 +0000

    Components created for each coin (#37)
    All good.

--- src/App.js
+++ src/App.js
@@ -1,92 +1,25 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import NewsPrice from "./Sections/NewsPrice";
+import LivePrice from "./Sections/LivePrice";
+import BitcoinGraph from "./Sections/BitcoinGraph";
+import EthereumGraph from "./Sections/EthereumGraph";
+import XrpGraph from "./Sections/XrpGraph";
+import EosGraph from "./Sections/EosGraph";
+import LitecoinGraph from "./Sections/LitecoinGraph";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="Coinlib-tracker">
-          <div className="Coinlib-tracker-child">
-            <iframe title="Coinlib-tracker-title" className="Coinlib-tracker-iframe" src="https://widget.coinlib.io/widget?type=horizontal_v2&theme=dark&pref_coin_id=1508&invert_hover=no" width="100%" height="36" scrolling="auto" marginWidth="0" marginHeight="0" frameBorder="0" border="0">
-            </iframe></div>
-          <div className="Coinlib-tracker-bottom">
-            powered by&nbsp;
-            <a href="https://coinlib.io" target="_blank" rel="noopener noreferrer" className="Coinlib-tracker-link">
-                      Coinlib
-            </a>
-          </div>
-        </div>
+        <NewsPrice />
         <h2>Koinalysis</h2>
-        <header className="App-header">
-          <div className="Dashboard">
-            <div className="Dashboard-child">
-              <iframe title="Coinlib-iframe" className="Dashboard-iframe" src="https://widget.coinlib.io/widget?type=full_v2&theme=dark&cnt=5&pref_coin_id=1508&graph=yes" width="100%" height="409" scrolling="auto" marginWidth="0" marginHeight="0" frameBorder="0" border="0">
-              </iframe>
-            </div>
-            <div className="Dashboard-bottom">Koinalysis powered by&nbsp;<a href="https://coinlib.io" className="Coinlib-link" target="_blank" rel="noopener noreferrer">Coinlib</a>
-            </div>
-          </div>
-        </header>
-        <div className="CurrencyWidget">
-          <div className="CurrencyWidget-child">
-            <iframe title="CoinlibWidget-iframe" src="https://widget.coinlib.io/widget?type=chart&theme=dark&coin_id=859&pref_coin_id=1508" width="100%" height="536" scrolling="auto" marginWidth="0" marginHeight="0" frameBorder="0" border="0" className="CurrencyWidget-iframe">
-            </iframe>
-          </div>
-          <div className="CurrencyWidget-bottom">
-            Koinalysis powered by&nbsp;
-            <a href="https://coinlib.io" target="_blank" rel="noopener noreferrer" className="Coinlib-link">
-              Coinlib
-            </a>
-          </div>
-        </div>
-        <div className="CurrencyWidget">
-          <div className="CurrencyWidget-child">
-            <iframe title="CoinlibWidget-iframe" src="https://widget.coinlib.io/widget?type=chart&theme=dark&coin_id=145&pref_coin_id=1508" width="100%" height="536" scrolling="auto" marginWidth="0" marginHeight="0" frameBorder="0" border="0" className="CurrencyWidget-iframe">
-            </iframe>
-          </div>
-          <div className="CurrencyWidget-bottom">
-            Koinalysis powered by&nbsp;
-            <a href="https://coinlib.io" target="_blank" rel="noopener noreferrer" className="Coinlib-link">
-              Coinlib
-            </a>
-          </div>
-        </div>
-        <div className="CurrencyWidget">
-          <div className="CurrencyWidget-child">
-            <iframe title="CoinlibWidget-iframe" src="https://widget.coinlib.io/widget?type=chart&theme=dark&coin_id=619&pref_coin_id=1508" width="100%" height="536" scrolling="auto" marginWidth="0" marginHeight="0" frameBorder="0" border="0" className="CurrencyWidget-iframe">
-            </iframe>
-          </div>
-          <div className="CurrencyWidget-bottom">
-            Koinalysis powered by&nbsp;
-            <a href="https://coinlib.io" target="_blank" rel="noopener noreferrer" className="Coinlib-link">
-              Coinlib
-            </a>
-          </div>
-        </div>
-        <div className="CurrencyWidget">
-          <div className="CurrencyWidget-child">
-            <iframe title="CoinlibWidget-iframe" src="https://widget.coinlib.io/widget?type=chart&theme=dark&coin_id=585&pref_coin_id=1508" width="100%" height="536" scrolling="auto" marginWidth="0" marginHeight="0" frameBorder="0" border="0" className="CurrencyWidget-iframe">
-            </iframe>
-          </div>
-          <div className="CurrencyWidget-bottom">
-            Koinalysis powered by&nbsp;
-            <a href="https://coinlib.io" target="_blank" rel="noopener noreferrer" className="Coinlib-link">
-              Coinlib
-            </a>
-          </div>
-        </div>
-        <div className="CurrencyWidget">
-          <div className="CurrencyWidget-child">
-            <iframe title="CoinlibWidget-iframe" src="https://widget.coinlib.io/widget?type=chart&theme=dark&coin_id=359&pref_coin_id=1508" width="100%" height="536" scrolling="auto" marginWidth="0" marginHeight="0" frameBorder="0" border="0" className="CurrencyWidget-iframe">
-            </iframe>
-          </div>
-          <div className="CurrencyWidget-bottom">
-            Koinalysis powered by&nbsp;
-            <a href="https://coinlib.io" target="_blank" rel="noopener noreferrer" className="Coinlib-link">
-              Coinlib
-            </a>
-          </div>
-        </div>
+        <LivePrice />
+        <BitcoinGraph />
+        <EthereumGraph />
+        <XrpGraph />
+        <EosGraph />
+        <LitecoinGraph />
       </div>
     );
   }
